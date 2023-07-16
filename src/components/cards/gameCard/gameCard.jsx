cb8a31e0dd70729814011bcb5e1461ce00893025
super312 <brunocesarmeneguzzuzamarchi@gmail.com>
2023-07-16 16:27:04 -0300

    fim da aula 2

--- src/components/cards/gameCard/gameCard.jsx
+++ src/components/cards/gameCard/gameCard.jsx
@@ -6,7 +6,7 @@ import Button from '@/components/forms/button/button'
 export default function GameCard() {
     return (
         <div className={styles.gamecard}>
-            <Image src='/products/counter-strike.jpg' width={300} height={145} />
+            <Image className={styles.image} src='/products/counter-strike.jpg' width={300} height={145} />
             <div className={styles.info}>
                 <h3 className={styles.title}>Counter strike: Global offensive</h3>
                 <p className={styles.categories}>ação, estratégia, multijogador</p>
