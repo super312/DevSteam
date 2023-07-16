c9a4d87491760fb9f087225193da98fc3cb0e4d0
super312 <brunocesarmeneguzzuzamarchi@gmail.com>
2023-07-16 15:23:57 -0300

    criado gameCard e modificado botao

--- src/components/cards/saleCard/saleCard.jsx
+++ src/components/cards/saleCard/saleCard.jsx
@@ -16,7 +16,7 @@ export default function SaleCard() {
                         <h4 className={styles.discountprice}>R$89,90</h4>
                     </div>
                 </div>
-                <Button>ADICIONAR AO CARRINHO</Button>
+                <Button fullwidth>ADICIONAR AO CARRINHO</Button>
             </div>
         </div>
     )
--- src/components/forms/button/button.jsx
+++ src/components/forms/button/button.jsx
@@ -1,7 +1,7 @@
 import styles from './button.module.css'
 
-export default function Button({ children }) {
+export default function Button({ children, fullwidth }) {
     return (
-        <button className={styles.button}>{ children }</button>
+        <button className={`${styles.button} ${fullwidth && styles.fullwidth}`}>{ children }</button>
     )
 }
