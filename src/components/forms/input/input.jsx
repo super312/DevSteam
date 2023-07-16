ead63822d9898ec2e3a25dcaae561e308578b813
super312 <brunocesarmeneguzzuzamarchi@gmail.com>
2023-07-16 15:43:58 -0300

    criado input e navbar

---
+++ src/components/forms/input/input.jsx
@@ -0,0 +1,10 @@
+import styles from './input.module.css'
+
+export default function Input({ fullwidth,...props }) {
+    return(
+        <input 
+        className={`${styles.input} ${fullwidth && styles.fullwidth}`}
+        {...props}
+        />
+    )
+}
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,15 @@
 import styles from './navbar.module.css'
 import { BsCart4 } from 'react-icons/bs'
 import Logo from '@/components/logo/logo'
+import Input from '@/components/forms/input/input'
 
 export default function Navbar(){
     return (
         <nav class={styles.navbar}>
             <Logo />
-            <input />
+            <div className={styles.search}>
+            <Input placeholder="Buscar" fullwidth/>
+            </div>
             <BsCart4 size={40} />
         </nav>
     )
